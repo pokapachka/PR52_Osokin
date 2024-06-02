b221dd06eeea83d5fedda542a19d03f5ed7e3ca7
pokapachka <pachkaosokin@yandex.ru>
2024-06-02 15:59:57 +0500

    Программирование окна MainWindow. Осокин

--- Items/Student.xaml.cs
+++ Items/Student.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ПР52_Осокин.Classes;
 
 namespace ПР52_Осокин.Items
 {
@@ -18,9 +19,39 @@ namespace ПР52_Осокин.Items
     /// </summary>
     public partial class Student : UserControl
     {
-        public Student()
+        public static Pages.Main _Main;
+        int id;
+        public Student(StudentContext student, Pages.Main Main)
         {
             InitializeComponent();
+            TBFio.Text = $"{student.Lastname} {student.Firstname}";
+            CBExpelled.IsChecked = student.Expelled;
+            List<DisciplineContext> StudentDisciplines = Main.AllDisciplines.FindAll(x => x.IdGroup == student.IdGroup);
+            int NecessarilyCount = 0;
+            int WorksCount = 0;
+            int DoneCount = 0;
+            int MissedCount = 0;
+            foreach (DisciplineContext StudentDiscipline in StudentDisciplines)
+            {
+                List<WorkContext> StudentWorks = Main.AllWorks.FindAll(x => (x.IdType == 1 || x.IdType == 2 || x.IdType == 3) && x.IdDiscipline == StudentDiscipline.Id);
+                NecessarilyCount += StudentWorks.Count;
+                foreach (WorkContext StudentWork in StudentWorks)
+                {
+                    EvaluationContext Evaulation = Main.AllEvaluations.Find(x => x.IdWork == StudentWork.Id && x.IdStudent == student.Id);
+                    if (Evaulation != null && Evaulation.Value.Trim() != "" && Evaulation.Value.Trim() != "2") DoneCount++;
+                }
+                StudentWorks = Main.AllWorks.FindAll(x =>
+                x.IdType != 4 && x.IdType != 3);
+                WorksCount += StudentWorks.Count;
+                foreach (WorkContext StudentWork in StudentWorks)
+                {
+                    EvaluationContext Evaluation = Main.AllEvaluations.Find(x => x.IdWork == StudentWork.Id && x.IdStudent == student.Id);
+                    if (Evaluation != null && Evaluation.Lateness.Trim() != "") MissedCount += Convert.ToInt32(Evaluation.Lateness);
+                }
+            }
+            doneWorks.Value = (100f / (float)NecessarilyCount) * ((float)DoneCount);
+            missedCount.Value = (100f / ((float)WorksCount * 90f)) * ((float)MissedCount);
+            TBGroup.Text = Main.AllGroups.Find(x => x.Id == student.IdGroup).Name;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace ПР52_Осокин
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(new Pages.Main());
         }
     }
 }
